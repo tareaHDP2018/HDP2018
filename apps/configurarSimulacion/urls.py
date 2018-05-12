34c93692c436017d9beacdb5d5c91a540f698402
dixon28 <dxam15005@gmail.com>
2018-05-11 22:26:37 -0600

    modificando login y registrar

--- apps/configurarSimulacion/urls.py
+++ apps/configurarSimulacion/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url,include
 from apps.configurarSimulacion.views import nuevo,configuracion,index
-
+app_name='configurarSimulacion'
 urlpatterns=[
 url(r'^$',index),
  url(r'^nuevo$',nuevo,name='nuevo'),

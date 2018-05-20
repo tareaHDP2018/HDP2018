b0dc93b2695f2f42df3c750ad64506d4822e777c
Huiza <mejia_huiza@hotmail.com>
2018-05-20 13:07:47 -0600

    Mis cambios al archivo consultar

--- apps/consultar/urls.py
+++ apps/consultar/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url,include
-from apps.consultar.views import simulacionlist
+from apps.consultar.views import simulacionlist,simulacion_editar,simulacion_eliminar
 
 urlpatterns=[
  url(r'^simulaciones$',simulacionlist,name='consultar'),
+ url(r'^editar/(?P<id_simulacion>\d*)/$',simulacion_editar,name='editar'),
+  url(r'^eliminar/(?P<id_simulacion>\d*)/$',simulacion_eliminar,name='eliminar'),
 
 ]
